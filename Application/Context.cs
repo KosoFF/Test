25c0a48f3eac1ef7075fabfd9729afb5c1c6ed5f
georgerogava <georgerogava>
2016-01-11 17:27:33 +0300

    enable migrations

---
+++ Application/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    class Context : DbContext
+    {
+        public DbSet <Models> Models { get; set; }
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+        public DbSet<Thing> Things { get; set; }
+        //public DbSet<Models> Models { get; set; }
+    }
+}
